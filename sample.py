7f71435cf9685521f9de9d6ffb226879ae8cfe4a
sushi4me <nathankong1688@yahoo.com>
2019-03-16 14:25:37 -0700

    Refactoring

--- sample.py
+++ sample.py
@@ -1,9 +1,8 @@
-from Exercises.daily_coding_problems import DailyCodingProblem
-from Exercises.exercise_problems import ExerciseProblems
+from exercises.daily_coding_problems import DailyCodingProblem
+from exercises.exercise_problems import ExerciseProblems
 
 if __name__ == "__main__":
     e = ExerciseProblems()
     dcp = DailyCodingProblem()
 
-    assert(dcp.parse_directory("dir\n\tsubdir1\n\tsubdir2\n\t\tfile.ext") == 20)
-    
+    print(dcp.max_subarray([9, 7, 10, 2, 3, 5, 6], 3))
